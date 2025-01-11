0bbc9e69f516d3ca292aafc1c3515ea9d2948727
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-01-11 13:23:09 +0000

    Update connectMetaMask.js

--- js/connectMetaMask.js
+++ js/connectMetaMask.js
@@ -5,9 +5,8 @@ const tokenSymbol = 'CDS';
 const tokenDecimals = 8;
 const tokenImage = 'https://i.imgur.com/ZXf2SKw.png';
 
-/** Connect to Crypto Development Services and the BSC mainnet network */
+/** Connect to Crypto Development Services and the Polygon mainnet network */
 const setupPolygonChain = async () => {
-  /** In case we need to throw an error, let's grab the error modal & error message */
   const errorModalContainer = document.querySelector('.error-modal-container');
   const errorMessage = document.querySelector('.error-message');
 
@@ -22,29 +21,30 @@ const setupPolygonChain = async () => {
             chainName: 'Polygon (Mainnet)',
             nativeCurrency: {
               name: 'Polygon (MATIC)',
-              symbol: 'POL',
+              symbol: 'MATIC',
               decimals: 18,
             },
-            rpcUrls: ['polygon-mainnet.infura.io'],
-            blockExplorerUrls: [
-              'https://polygonscan.com/',
-            ],
+            rpcUrls: ['https://polygon-rpc.com/'], // Public RPC or Infura
+            blockExplorerUrls: ['https://polygonscan.com/'],
           },
         ],
       });
-      await provider.request({ method: 'wallet_watchAsset',
-      params: {
-        type: 'ERC20', // Initially only supports ERC20, but eventually more!
-        options: {
-          address: tokenAddress, // The address that the token is at.
-          symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
-          decimals: tokenDecimals, // The number of decimals in the token
-          image: tokenImage, // A string url of the token logo
+      const wasAdded = await provider.request({
+        method: 'wallet_watchAsset',
+        params: {
+          type: 'ERC20',
+          options: {
+            address: tokenAddress,
+            symbol: tokenSymbol,
+            decimals: tokenDecimals,
+            image: tokenImage,
+          },
         },
-      },
-     })
+      });
+      if (!wasAdded) {
+        console.log('Token addition rejected by the user.');
+      }
     } catch (e) {
-      /** Code 4001 is user rejected, we don't need to notify the user if they rejected the request */
       if (e.code !== 4001) {
         errorModalContainer.style.display = 'block';
         errorMessage.innerHTML = e.message;
@@ -56,61 +56,38 @@ const setupPolygonChain = async () => {
   }
 };
 
-/**  Add event listener to the Connect MetaMask buttons */
+/** Add event listener to the Connect MetaMask button */
 const connectMetaMask = document.querySelector('.connectMetaMask');
-const connectMetaMaskNav = document.querySelector('.connectMetaMask-nav');
 
-// If user is not on Integrate MetaMask page, connectMetaMask will not be available so
-// we need to check if it's there before adding the event listener to it
- if (connectMetaMask) {
-  connectMetaMask.addEventListener('click', () => {
-    setupPolygonChain();
-  });
+if (connectMetaMask) {
+  connectMetaMask.addEventListener('click', setupPolygonChain);
 }
-//  } if (connectMetaMask) {
-// connectMetaMaskNav.addEventListener('click', () => {
-//   setupPolygonChain();
-// });
-//  }
-/** If we are already connected to Crypto Development Services, show disbled button with 'Connected' text */
-const connectButtons = [connectMetaMask/*, connectMetaMaskNav*/];
+
+/** Display "Connected" button text if already connected */
 const displayConnectedButton = async () => {
-  const accounts = await ethereum.request({ method: 'eth_accounts' });
-  connectButtons.forEach((button) => {
-    if (button && accounts.length > 0) {
-      const shortenedAccount = `${accounts[0].slice(
-        0,
-        6
-      )}...${accounts[0].slice(-4)}`;
-      button.innerHTML = `Connected: ${shortenedAccount}`;
-      button.className += ' disabled-button';
-      button.removeEventListener('click', () => {});
-    }
-  });
+  const accounts = await provider.request({ method: 'eth_accounts' });
+  if (accounts.length > 0) {
+    const shortenedAccount = `${accounts[0].slice(0, 6)}...${accounts[0].slice(-4)}`;
+    connectMetaMask.innerHTML = `Connected: ${shortenedAccount}`;
+    connectMetaMask.classList.add('disabled-button');
+    connectMetaMask.removeEventListener('click', setupPolygonChain);
+  }
 };
 
 const isConnectedToPolygonChain = async () => {
-  const chainId = await provider.request({
-    method: 'eth_chainId',
-  });
+  const chainId = await provider.request({ method: 'eth_chainId' });
   if (chainId === PolygonChainId) {
     displayConnectedButton();
   }
 };
 
 if (provider) {
-  /** Check if user is connected to Wrapped NewYorkCoin and display correct button text */
   isConnectedToPolygonChain();
 
-  /** Reload the page if the chain changes */
   provider.on('chainChanged', () => {
-    // MetaMask recommends reloading the page unless we have good reason not to
-    // Plus, everytime the window reloads, we call isConnectedToPolygonChain again
-    // and can show the correct 'Connected' or 'Connect MetaMask' button text
     window.location.reload();
   });
 
-  /** When the account changes update the button text */
   provider.on('accountsChanged', (accounts) => {
     if (accounts.length > 0) {
       displayConnectedButton();
